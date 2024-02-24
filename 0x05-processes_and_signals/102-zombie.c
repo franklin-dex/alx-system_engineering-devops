34abf8ca5581843653e3651c60ccf0ae804cc548
Frankiln <odinakachukwufranklin0@gmail.com>
2024-02-23 19:33:26 -0800

    add zombie task file

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+/**
+ * infinite_while - Creates an infinite loop for the parent process.
+ *
+ * Return: Always 0.
+ */
+int infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	pid_t child_pid;
+	int i;
+
+	for (i = 0; i < 5; i++)
+	{
+		child_pid = fork();
+
+		if (child_pid == -1)
+		{
+			perror("fork");
+			exit(EXIT_FAILURE);
+		}
+
+		if (child_pid == 0)
+		{
+			/*Child process*/
+			printf("Zombie process created, PID: %d\n", getpid());
+			exit(0);
+		}
+	}
+
+	/*parent process*/
+	infinite_while();
+
+	return (0);
+}
